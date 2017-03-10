1978014043cd4307c419d9345eae612a9b10ee42
Onur Erbilgin <oerbilgin@lbl.gov>
2017-03-09 21:51:23 -0800

    initial commit

---
+++ mass_spec_helpers.py
@@ -0,0 +1,34 @@
+sys.path.insert(1,'/global/project/projectdirs/metatlas/anaconda/lib/python2.7/site-packages' )
+from metatlas import metatlas_objects as metob
+
+from rdkit import Chem
+from rdkit.Chem import AllChem
+from rdkit.Chem import Draw
+from rdkit import DataStructs
+from rdkit.Chem.Fingerprints import FingerprintMols
+from rdkit.Chem import Descriptors
+
+def ppm_error(mass, theoretical_mass):
+    """
+    returns the ppm error of a given observed mass, and theoretical mass
+    """
+    theoretical_mass = Descriptors.ExactMolWt(mol)
+    ppm = (mass - theoretical_mass) / theoretical_mass * 1e6
+    return ppm
+
+def ppm_window(mass, ppm=5, result='bounds'):
+    """
+    given a mass and a ppm error, returns lower and upper bounds corresponding to that ppm window
+    result can be "bounds" or "error"
+    bounds is a list of lower and upper bounds
+    error is an amu value of the error, so you can add or subtract the error as you wish
+    """
+    error = ppm/1e6 * mass
+    lower_bound = mass - error
+    upper_bound = mass + error
+    if result.lower() == 'bounds':
+        return [lower_bound, upper_bound]
+    elif result.lower() == 'error':
+        return error
+    else:
+        raise RuntimeError('%s is not a valid result argument' %(result))
---
+++ plot_helpers.py
@@ -0,0 +1,40 @@
+from matplotlib import pyplot as pyplot
+%matplotlib inline
+
+def cmap_color(curve_idx, n_curves, color_map='viridis'):
+    """
+    returns the suitable color from the given color map for the plot number
+    
+    Inputs
+    ------
+    curve_idx: index position of the curve you are getting the plot for
+    n_curves: total number of curves you want to plot with the color map
+    color_map: a valid matplotlib color map. Available choices:
+    
+    cmaps = [('Perceptually Uniform Sequential',
+                            ['viridis', 'inferno', 'plasma', 'magma']),
+         ('Sequential',     ['Blues', 'BuGn', 'BuPu',
+                             'GnBu', 'Greens', 'Greys', 'Oranges', 'OrRd',
+                             'PuBu', 'PuBuGn', 'PuRd', 'Purples', 'RdPu',
+                             'Reds', 'YlGn', 'YlGnBu', 'YlOrBr', 'YlOrRd']),
+         ('Sequential (2)', ['afmhot', 'autumn', 'bone', 'cool',
+                             'copper', 'gist_heat', 'gray', 'hot',
+                             'pink', 'spring', 'summer', 'winter']),
+         ('Diverging',      ['BrBG', 'bwr', 'coolwarm', 'PiYG', 'PRGn', 'PuOr',
+                             'RdBu', 'RdGy', 'RdYlBu', 'RdYlGn', 'Spectral',
+                             'seismic']),
+         ('Qualitative',    ['Accent', 'Dark2', 'Paired', 'Pastel1',
+                             'Pastel2', 'Set1', 'Set2', 'Set3', 'Vega10',
+                             'Vega20', 'Vega20b', 'Vega20c']),
+         ('Miscellaneous',  ['gist_earth', 'terrain', 'ocean', 'gist_stern',
+                             'brg', 'CMRmap', 'cubehelix',
+                             'gnuplot', 'gnuplot2', 'gist_ncar',
+                             'nipy_spectral', 'jet', 'rainbow',
+                             'gist_rainbow', 'hsv', 'flag', 'prism'])]
+
+    """
+    cmap = plt.get_cmap('viridis')
+    c_norm = colors.Normalize(vmin=0, vmax=range(n_curves)[-1])
+    scalar_map = cmx.ScalarMappable(norm=c_norm, cmap=cmap)
+    color = scalarMap.to_rgba(curve_idx)
+    return color
