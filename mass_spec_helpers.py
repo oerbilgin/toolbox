a1d79d8ac170c808cf65c6f2c8d14166ec43fd62
Onur Erbilgin <oerbilgin@lbl.gov>
2017-03-20 14:44:02 -0700

    formatting

--- mass_spec_helpers.py
+++ mass_spec_helpers.py
@@ -1,5 +1,7 @@
 import sys
-sys.path.insert(1,'/global/project/projectdirs/metatlas/anaconda/lib/python2.7/site-packages' )
+sys.path.insert(1,
+    '/global/project/projectdirs/metatlas/anaconda/lib/python2.7/site-packages'
+    )
 from metatlas import metatlas_objects as metob
 
 import pandas as pd
@@ -13,7 +15,7 @@ from rdkit.Chem import Descriptors
 
 def ppm_error(mass, theoretical_mass):
     """
-    returns the ppm error of a given observed mass, and theoretical mass
+    Returns the ppm error of a given observed mass, and theoretical mass
     """
     if not isinstance(theoretical_mass, float):
         if isinstance(theoretical_mass, int):
@@ -25,10 +27,21 @@ def ppm_error(mass, theoretical_mass):
 
 def ppm_window(mass, ppm=5, result='bounds'):
     """
-    given a mass and a ppm error, returns lower and upper bounds corresponding to that ppm window
-    result can be "bounds" or "error"
-    bounds is a list of lower and upper bounds
-    error is an amu value of the error, so you can add or subtract the error as you wish
+    Given a mass and a ppm error, returns lower and upper bounds 
+    corresponding to that ppm window. 
+
+    Inputs
+    ------
+    mass:   monoisotopic mass
+    ppm:    the ppm error to construct the window
+    result: "bounds" returns a list of lower and upper bounds
+            "error" returns the amu value of the error, if you wanted to
+            add and/or subtract the error as you wish
+
+    Outputs
+    -------
+    Either a list of lower and upper mass bounds, or a single value
+    corresponding to the amu error
     """
     error = ppm/1e6 * mass
     lower_bound = mass - error
@@ -38,50 +51,73 @@ def ppm_window(mass, ppm=5, result='bounds'):
     elif result.lower() == 'error':
         return error
     else:
-        raise RuntimeError('%s is not a valid result argument' %(result))
+        raise RuntimeError(
+            '%s is not a valid result argument' %(result)
+            )
 
-def find_mass(target_weight, ppm, df, mz_column_name='mz', rt_column_name='rt_peak', rt=None, rt_bound=0.05):
+def find_mass(target_weight, ppm, df, 
+    mz_column_name='mz', rt_column_name='rt_peak', rt=None, rt_bound=0.05):
     """
-    given a target mass and ppm window, return the rows of a dataframe corresponding to the mass. 
+    Given a target mass and ppm window, return the rows of a dataframe 
+    corresponding to the mass. 
     Optionally enter a retention time and bound
     
     Inputs
     ------
-    target_weight: the m/z you are looking for
-    ppm: the ppm error you are willing to have
-    df: a dataframe with rows being individual features
-    mz_column_name: the column name that has the feature's m/z, metatlas default: "mz"
-    rt_column_name: the column name that has the reature's retention time peak, metatlas default: "rt_peak
-    rt: the retention time for the feature
-    rt_bound: the upper and lower bound you're willing to search for the retention time, default +/- 0.05 minutes
+    target_weight:  the m/z you are looking for
+    ppm:            the ppm error you are willing to have
+    df:             a dataframe with rows being individual features
+    mz_column_name: the column name that has the feature's m/z, metatlas
+                    default: "mz"
+    rt_column_name: the column name that has the reature's retention 
+                    time peak, metatlas default: "rt_peak
+    rt:             the retention time for the feature
+    rt_bound:       the upper and lower bound you're willing to search 
+                    for the retention time, default +/- 0.05 minutes
     
     Outputs
     -------
-    returns a slice of the input dataframe that only contains rows that matched the search parameters
+    A slice of the input dataframe that only contains rows that matched 
+    the search parameters
     """
     w = ppm_window(target_weight, ppm)
     a = df[(df[mz_column_name] >= w[0]) & (df[mz_column_name] <= w[1])]
     if rt is not None:
-        a = a[(a[rt_column_name] > rt - rt_bound) & (a[rt_column_name] < rt + rt_bound)]
+        a = a[(a[rt_column_name] > rt - rt_bound) & \
+              (a[rt_column_name] < rt + rt_bound)]
     return a
 
 def data_convert(row, data_pattern='Peak height', search='endswith'):
     """
-    Converts an input Series into a dataframe with 'x' and 'y' columns, with 'x' corresponding to run order, and 'y' corresponding to a measurement of interest
-    Run number is determined by splitting by the word "Run" and then by a period. For example: "experimentinfo_Run1.mzml"
-    If your filenames don't look like that, then the run number finder probably won't work
+    Converts an input Series into a dataframe with 'x' and 'y' columns, 
+    with 'x' corresponding to run order, and 'y' corresponding to a 
+    measurement of interest.
+
+    Run number is determined by splitting by the word "Run" and then by 
+    a period. For example: "experimentinfo_Run1.mzml"
+
+    If your filenames don't look like that, then the run number finder 
+    probably won't work
     
     Inputs
     ------
-    row: a Pandas Series object, where the colums contain run information
-    data_pattern: is the text pattern of the column that contains data you want to plot, default: "Peak height"
-    search: the Pandas string methods search type:
-        if search is "endswith" or "startswith" then it will be a str.endswith or str.startswith search; multiple data_patterns to search should be in a TUPLE
-        if search is "contains" then it will be a str.contains search; multiple data_patterns to search should be in one string separated by '|'
+    row:            a Pandas Series object, where the colums contain run
+                    information
+    data_pattern:   is the text pattern of the column that contains data
+                    you want to plot, default is "Peak height"
+    search:         the Pandas string methods search type:
+                    - if search is "endswith" or "startswith" then it 
+                    will be a str.endswith or str.startswith search; 
+                    multiple data_patterns to search should be in a 
+                    TUPLE
+                    - if search is "contains" then it will be a 
+                    str.contains search; multiple data_patterns to 
+                    search should be in one string separated by '|'
         
     Outputs
     -------
-    A dataframe with columns 'x' and 'y' that is sorted in ascending order of 'x'
+    A dataframe with columns 'x' and 'y' that is sorted in ascending 
+    order of 'x'
     """
     if not isinstance(row, pd.Series):
         raise RuntimeError('input was not a Series datatype!')
@@ -92,7 +128,9 @@ def data_convert(row, data_pattern='Peak height', search='endswith'):
     elif search.lower()=='contains':
         c = row.keys()[row.keys().str.contains(data_pattern)]
     else:
-        raise RuntimeError('Did not recognize %s as a valid Pandas search pattern, or it is not encompassed yet' %(search))
+        raise RuntimeError(
+            'Did not recognize %s as a valid Pandas search pattern, or it is \
+            not encompassed yet' %(search))
         
     y = row[c].values.tolist()
     x = []
@@ -105,9 +143,12 @@ def data_convert(row, data_pattern='Peak height', search='endswith'):
 
 def ctrl_expt_compare(medium, experimental, fold_change=4):
     """
-    Removes features that are within a window of detection for both medium control and an experimental condition.
-    Input dataframes are data_convert() outputs! (i.e. 'x' and 'y' columns corresponding to run number and ion intensity, respectively)
-    Compares an experimental data point with the medium controls that were run within a 20-run window (+/- 10 runs) of the experimental
+    Removes features that are within a window of detection for both 
+    medium control and an experimental condition.
+    Input dataframes are data_convert() outputs! (i.e. 'x' and 'y' 
+    columns corresponding to run number and ion intensity, respectively)
+    Compares an experimental data point with the medium controls that 
+    were run within a 20-run window (+/- 10 runs) of the experimental
     
     Keeps features that: 
     1. Are much higher in experimental than medium control
@@ -115,33 +156,47 @@ def ctrl_expt_compare(medium, experimental, fold_change=4):
     
     Inputs
     ------
-    medium: data_convert() output corresponding to medium control runs for one feature
-    experimental: data_convert() output corresponding to experimental condition runs for one feature
-    fold_change: boundary to toss a feature if found to be fold_change higher in experimental over medium, or 1/fold_change lower in experimental over medium; default 4
+    medium:         data_convert() output corresponding to medium 
+                    control runs for one feature
+    experimental:   data_convert() output corresponding to experimental
+                    condition runs for one feature
+    fold_change:    boundary to toss a feature if found to be 
+                    fold_change higher in experimental over medium, or 
+                    1/fold_change lower in experimental over medium; 
+                    default 4
     
     Outputs
     -------
-    boolean True or False, where True means that the feature is has a very different intensity (as judged by fold_change) in experimental compared to the nearest control run
+    boolean True or False, where True means that the feature is has a 
+    very different intensity (as judged by fold_change) in experimental
+    compared to the nearest control run
     """
-    # for each medium control point, look for experimental points within 10 runs of a control point
+    # for each medium control point, look for experimental points within
+    # 10 runs of a control point
     for idx, ctrl in medium.iterrows():
-        expt = experimental[(experimental['x'] < (ctrl['x']+10)) & (experimental['x'] > (ctrl['x']-10))]   
+        expt = experimental[(experimental['x'] < (ctrl['x']+10)) & \
+                            (experimental['x'] > (ctrl['x']-10))]   
         # if there is an experimental point within 20 of a control
         if len(expt) > 0:
             for y in expt['y']:
-                # if the control is zero, don't chuck it regardless of what experimental is
+                # if the control is zero, don't chuck it regardless of 
+                # what experimental is
                 if ctrl['y'] == 0.:
                     continue
-                # otherwise, look at the fold difference between experimental and control
+                # otherwise, look at the fold difference between 
+                # experimental and control
                 else:
                     fold = y / ctrl['y']
-                    # if the experimental is greater than 4x the control or less than .25x of the control, dont chuck it
+                    # if the experimental is greater than 4x the control
+                    # or less than .25x of the control, dont chuck it
                     if fold > fold_change or fold < (1 / fold_change):
                         continue
-                    # if the experimental is ever within a 0.25 - 4x range of the control, toss it immediately
+                    # if the experimental is ever within a 0.25 - 4x 
+                    #range of the control, toss it immediately
                     else:
                         return False
-    # if the experimental is always less than 1/4x of control or greater than 4x of control across all control data points, then keep it
+    # if the experimental is always less than 1/4x of control or greater
+    # than 4x of control across all control data points, then keep it
     return True
 
 def cpd_from_inchikey(inchikey, blocks=1):
@@ -150,11 +205,12 @@ def cpd_from_inchikey(inchikey, blocks=1):
     
     Inputs
     ------
-    inchikey: a standard inchikey, e.g. RYYVLZVUVIJVGH-UHFFFAOYSA-N
-    blocks: which blocks of the inchikey to use as a search pattern to find compound(s) in metatlas
-        1: RYYVLZVUVIJVGH
-        2: RYYVLZVUVIJVGH-UHFFFAOYSA
-        3: RYYVLZVUVIJVGH-UHFFFAOYSA-N
+    inchikey:   a standard inchikey, e.g. RYYVLZVUVIJVGH-UHFFFAOYSA-N
+    blocks:     which blocks of the inchikey to use as a search pattern 
+                to find compound(s) in metatlas, example:
+                1: RYYVLZVUVIJVGH
+                2: RYYVLZVUVIJVGH-UHFFFAOYSA
+                3: RYYVLZVUVIJVGH-UHFFFAOYSA-N
 
     '''
     if blocks not in [1,2,3]:
@@ -162,7 +218,8 @@ def cpd_from_inchikey(inchikey, blocks=1):
 
     if (inchikey != '') and (inchikey is not None):
         search_pattern = '-'.join(inchikey.split('-')[:blocks])
-        cpds = metob.retrieve('Compounds', inchi_key='%s%%'%(inchikey.split('-')[0]), username='*')
+        cpds = metob.retrieve('Compounds', inchi_key='%s%%' \
+                                %(inchikey.split('-')[0]), username='*')
         if len(cpds) != 0:
             return cpds[0].name
         else:
